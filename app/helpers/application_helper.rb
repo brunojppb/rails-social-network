17ccc065df8f87ffd82fc455fe0862ee699b039d
Bruno Paulino <brunojppb@gmail.com>
2015-04-27 11:22:56 -0400

    Adds full_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = 'Social Net'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | Social Net"
+    end
+  end
+
 end
