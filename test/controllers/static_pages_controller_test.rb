3c1d9a9381f6a2bb3c5410379d4ea3d6b7c1a3e8
Bruno Paulino <brunojppb@gmail.com>
2015-04-27 13:17:28 -0400

    Finishes layout using bootstrap and static routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Social Net"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Social Net"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Social Net"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Social Net"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "sould get about" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
